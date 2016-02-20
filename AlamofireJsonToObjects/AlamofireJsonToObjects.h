55d08854954a05f1030d1361f62991839964530d
Dave <davidwu226@hotmail.com>
2016-02-19 18:24:12 -0800

    Add support for Carthage.

---
+++ AlamofireJsonToObjects/AlamofireJsonToObjects.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireJsonToObjects.h
+//  AlamofireJsonToObjects
+//
+//  Created by David Wu on 2/19/16.
+//  Copyright © 2016 evict. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlamofireJsonToObjects.
+FOUNDATION_EXPORT double AlamofireJsonToObjectsVersionNumber;
+
+//! Project version string for AlamofireJsonToObjects.
+FOUNDATION_EXPORT const unsigned char AlamofireJsonToObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireJsonToObjects/PublicHeader.h>
+
+
