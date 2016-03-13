722b0c7c5f05f2e345123910f784aacec0b59e2e
Tamir Shomer <tamir7@gmail.com>
2016-03-13 13:53:16 +0200

    Added encoding optional param to responseObject and responseArray functions

--- AlamofireJsonToObjects/AlamofireJsonToObjects.swift
+++ AlamofireJsonToObjects/AlamofireJsonToObjects.swift
@@ -15,12 +15,13 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
+    - parameter encoding: The string encoding. If `nil`, the string encoding will be determined from the server response, falling back to the default HTTP default character set,ISO-8859-1.
     - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response object (of type Mappable) and any error produced while making the request
     
     - returns: The request.
     */
-    public func responseObject<T:EVObject>(completionHandler: (Result<T, NSError>) -> Void) -> Self {
-        return responseObject(nil) { (request, response, data) in
+    public func responseObject<T:EVObject>(encoding: NSStringEncoding? = nil, completionHandler: (Result<T, NSError>) -> Void) -> Self {
+        return responseObject(encoding: encoding) { (request, response, data: Alamofire.Result<T, NSError>) -> Void in
             completionHandler(data)
         }
     }
@@ -28,26 +29,14 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
-    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response object (of type Mappable), the raw response data, and any error produced making the request.
-    
-    - returns: The request.
-    */
-    public func responseObject<T:EVObject>(completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<T, NSError>) -> Void) -> Self {
-        return responseObject(nil) { (request, response, data) in
-            completionHandler(request, response, data)
-        }
-    }
-    
-    /**
-    Adds a handler to be called once the request has finished.
-    
     - parameter queue: The queue on which the completion handler is dispatched.
+    - parameter encoding: The string encoding. If `nil`, the string encoding will be determined from the server response, falling back to the default HTTP default character set,ISO-8859-1.
     - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response object (of type Mappable), the raw response data, and any error produced making the request.
     
     - returns: The request.
     */
-    public func responseObject<T:EVObject>(queue: dispatch_queue_t?, completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<T, NSError>) -> Void) -> Self {
-        return responseString(completionHandler: { (response) -> Void in
+    public func responseObject<T:EVObject>(queue: dispatch_queue_t? = nil, encoding: NSStringEncoding? = nil, completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<T, NSError>) -> Void) -> Self {
+        return responseString(encoding: encoding, completionHandler: { (response) -> Void in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
                 dispatch_async(queue ?? dispatch_get_main_queue()) {
                     switch response.result {
@@ -67,12 +56,13 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
+    - parameter encoding: The string encoding. If `nil`, the string encoding will be determined from the server response, falling back to the default HTTP default character set,ISO-8859-1.
     - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response array (of type Mappable) and any error produced while making the request
     
     - returns: The request.
     */
-    public func responseArray<T:EVObject>(completionHandler: (Alamofire.Result<[T], NSError>) -> Void) -> Self {
-        return responseArray { (request, response, data) -> Void in
+    public func responseArray<T:EVObject>(encoding: NSStringEncoding? = nil, completionHandler: (Alamofire.Result<[T], NSError>) -> Void) -> Self {
+        return responseArray(encoding: encoding) { (request, response, data: Alamofire.Result<[T], NSError>) -> Void in
             completionHandler(data)
         }
     }
@@ -80,24 +70,14 @@ extension Request {
     /**
     Adds a handler to be called once the request has finished.
     
-    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response array (of type Mappable), the raw response data, and any error produced making the request.
-    
-    - returns: The request.
-    */
-    public func responseArray<T:EVObject>(completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<[T], NSError>) -> Void) -> Self {
-        return responseArray(nil, completionHandler: completionHandler)
-    }
-    
-    /**
-    Adds a handler to be called once the request has finished.
-    
     - parameter queue: The queue on which the completion handler is dispatched.
+    - parameter encoding: The string encoding. If `nil`, the string encoding will be determined from the server response, falling back to the default HTTP default character set,ISO-8859-1.
     - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response array (of type Mappable), the raw response data, and any error produced making the request.
     
     - returns: The request.
     */
-    public func responseArray<T:EVObject>(queue: dispatch_queue_t?, completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<[T], NSError>) -> Void) -> Self {
-        return responseString(completionHandler: { (response) -> Void in
+    public func responseArray<T:EVObject>(queue: dispatch_queue_t? = nil, encoding: NSStringEncoding? = nil, completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<[T], NSError>) -> Void) -> Self {
+        return responseString(encoding: encoding, completionHandler: { (response) -> Void in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
                 dispatch_async(queue ?? dispatch_get_main_queue()) {
                     switch response.result {
